5bce9d5b2ff74c0d7cfca9bb55962b9669e648dd
Li <3337215188@qq.com>
2024-10-07 17:15:04 +0800

    删除Debug模块中的print类

--- Debug/src/Main.java
+++ Debug/src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        InterfaceA a = new Print();
-        a.printCapitalLetter();
-        InterfaceB b = new Print();
-        b.printLowercaseLetter();
+
     }
 }
